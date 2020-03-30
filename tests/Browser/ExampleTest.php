3f84b2d5513af3c4c6ccb883cd2ee950e2792e59
SID400 <p200300@mail.ru>
2020-03-30 14:38:26 +0300

    add DUSK Create test for create news form

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTest extends DuskTestCase
+class phpExampleTest extends DuskTestCase
 {
     /**
      * A basic browser test example.
